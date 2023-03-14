f412614c240941d58c128f5d51c389e6a2b43fdb
stack-usman <usmans.neslit@gmail.com>
2023-03-14 20:54:31 +0500

    changes

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,8 +1,11 @@
-<h1>{{ title }}</h1>
-<app-heroes></app-heroes>
+export interface Hero {
+    id: number;
+    name: string;
+}
 
-<!-- 
+
+/*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at https://angular.io/license
--->
+*/
---
+++ src/app/mock.heroes.ts
@@ -0,0 +1,14 @@
+
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Superman' },
+    { id: 2, name: 'Batman' },
+    { id: 3, name: 'Celeritas' },
+    { id: 4, name: 'Magneta' },
+    { id: 5, name: 'RubberMan' },
+    { id: 6, name: 'Dynama' },
+    { id: 7, name: 'Dr. IQ' },
+    { id: 8, name: 'Magma' },
+    { id: 9, name: 'Tornado' }
+]
